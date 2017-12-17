27f2755bd2095184e438215d2b507e304cbf55e3
Glen Little <glen.little@gmail.com>
2017-12-17 01:22:21 -0700

    console app not working yet... no .EXE file generated!

---
+++ src/WondrousNodaTimeDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using WondrousNodaTime;
+
+namespace WondrousNodaTimeDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine($"Today is {new WondrousDate().ToString("{W} ({G})")}");
+
+      var yi = new WondrousYearInfo(174);
+      var nawRuz = yi.GetSpecialDays(SpecialDayType.HolyDay_WorkSuspended, HolyDayCode.NawRuz).First();
+
+      Console.WriteLine($"Naw Ruz was on {nawRuz.Date.ToString("{W} ({G})")}");
+    }
+  }
+}
